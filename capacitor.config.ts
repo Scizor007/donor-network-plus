ce8b35ee6ed306cb3cda26ff437031208b29fd7c
Mubashira Shaik <shaikmubashira2006@gmail.com>
2025-09-21 11:51:36 +0530

    icon of app updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bloodbridge.app',
   appName: 'Blood Bridge',
-  webDir: "dist",
+  webDir: 'dist',
 };
 
 export default config;
